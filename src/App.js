0062edb60f5360b0913f338303c53f80d06da034
noranlenna <noranlenna@gmail.com>
2022-07-12 15:23:07 +0900

    6. 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Hello from "./component/Hello";
 import Wrapper from "./component/Wrapper";
 
-
+// props 값을 생략하면 true
+// isSpecial={true}
 function App() {
   return (
     <div className="App">
       <Wrapper>
-    <Hello name={"React"} color={{backgroundColor : "red"}}></Hello>
+    <Hello name={"React"} color={{backgroundColor : "red"}} isSpecial></Hello>
       <Hello></Hello>
 
       </Wrapper>
--- src/component/Hello.jsx
+++ src/component/Hello.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+// 삼항연산자 사용해도 되지만,
+// undefined, null, false 는 렌더하지 않으므로,
+// && 연산자를 사용해서 처리하는 것이 간편하다.
+// 단축 평가 논리 계산법
 
-
-function Hello({color,name}) {
+function Hello({color,name, isSpecial}) {
 	return (
 		<div>
 			<div style={color}>
+				{isSpecial && <b>*</b>}
 				안녕하세요 {name}
 			</div>
 
