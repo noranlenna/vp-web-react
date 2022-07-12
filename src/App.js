59ca1e32af95d27b7b817de2fe01c72151d85055
noranlenna <noranlenna@gmail.com>
2022-07-12 19:24:50 +0900

    14.배열에 항목 제거하기

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import UserList from "./component/UserList";
 import { useRef, useState } from "react";
 import CreateUser from "./component/CreateUser";
 
-// 배열에 변화를 줄 땐, 객체와 마찬가지로 불변성을 지켜야 한다.
-// 배열의 Push, Splice, Sort 등 직접 수정하는 함수를 그냥 사용해선 안된다.
-// 사용할 경우에는 스프레드 문법을 이용하여 객체를 복사한 뒤, 위 함수를 사용하거나,
-// 새로운 배열을 리턴하는 함수를 사용한다.
-
 function App() {
   // focus 추가
   const focusRef = useRef("");
@@ -51,9 +46,7 @@ function App() {
       username,
       email,
     };
-
     setUsers(users.concat(user));
-    // setUsers([...users,user]); 배열 복사
 
     setInputs({
       username: "",
@@ -61,6 +54,14 @@ function App() {
     });
     nextId.current += 1;
     focusRef.current.focus();
+  };
+
+  const handleOnRemove = (id) => {
+    setUsers(
+      users.filter((user) => {
+        return user.id !== id;
+      })
+    );
   };
 
   return (
@@ -73,7 +74,7 @@ function App() {
           handleOnCreate={handleOnCreate}
           focusRef={focusRef}
         ></CreateUser>
-        <UserList users={users}></UserList>
+        <UserList users={users} handleOnRemove={handleOnRemove}></UserList>
       </>
     </div>
   );
--- src/component/UserList.jsx
+++ src/component/UserList.jsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import React from "react";
 
 
+// onClick={extenalFunc()} 형식으로, 함수 자체를 실행시키면,
+// 컴포넌트가 렌더링 되는 동시에 실행이 되버린다.
+// 그래서 ()를 빼서 즉시실행 시키기 않도록 한다.
+// 하지만, function에 파라메터가 필요할 땐,
+// onClick={paramFunc(somePara)} 형태로 하면 바로 실행이 되버리므로,
+// onClick에 콜백 함수로 처리를 하고(Arrow Function), 함수가 실행될 때,
+// user.id를 건네주어 처리하는 방식으로 한다.
 
-function User({ user }) {
-	return (
-		<div>
-			<b>{user.username}</b> <span>({user.email})</span>
-		</div>
-	);
+function User({ user, handleOnRemove }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+      <button
+        type="button"
+        onClick={() => {
+          handleOnRemove(user.id);
+        }}
+      >
+        삭제
+      </button>
+    </div>
+  );
 }
 
-
-function UserList({users}) {
-	return (
-		<div>
-			{users.map(user => (
-				<User user={user} key={user.id}/>
-			))}
-		</div>
-	);
+function UserList({ users, handleOnRemove }) {
+  return (
+    <div>
+      {users.map((user) => (
+        <User user={user} key={user.id} handleOnRemove={handleOnRemove} />
+      ))}
+    </div>
+  );
 }
 
 export default UserList;
