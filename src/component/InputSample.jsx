0077bea4b3c6a4a87c062c8c81ae6c25c01a8f14
noranlenna <noranlenna@gmail.com>
2022-07-12 16:36:38 +0900

    10.useRef로 특정 DOM 선택하기

--- src/component/InputSample.jsx
+++ src/component/InputSample.jsx
@@ -1,16 +1,12 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
-
-// input의 개수가 여러개가 됐을 경우, useState를 여러번 사용해선 안된다.
-// input에 name을 설정하고, 이벤트가 발생할 떄, 값을 참조하도록 한다.
-// useState는 객체 형태로 관리해야한다.
 
 function InputSample() {
-
 	const [inputs, setInputs] = useState({
 		name:'',
 		nickname:''
 	});
+	const nameInput = useRef()
 
 	const {name, nickname}= inputs; // 비구조화 할당
 
@@ -27,15 +23,14 @@ function InputSample() {
 			name:'',
 			nickname: ''
 		})
+		nameInput.current.focus();
 
 	}
 
-	// 리액트는 객체를 직접 수정해선 안된다.
-	// 새로운 객체를 만든 뒤, 해당 객체에 변화를 주고, 그 객체를 상태로 사용한다.
-
+	// ref 객체를 만들고, 선택하고 싶은 dom에 ref 프로퍼티 값을 설정한다.
 	return (
 		<div>
-			<input type="text" name="name" value={name} placeholder="이름" onChange={handleOnChange}/>
+			<input type="text" name="name" value={name} placeholder="이름" onChange={handleOnChange} ref={nameInput}/>
 			<input type="text" name="nickname" value={nickname} id="" placeholder="닉네임" onChange={handleOnChange}/>
 			<button type="button" onClick={handleOnReset}>초기화</button>
 			<div className="">
