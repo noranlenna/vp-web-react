4050f5b29f720b8f610978b26939ab7f75fb434a
noranlenna <noranlenna@gmail.com>
2022-07-12 17:10:24 +0900

    12.useRef로 컴포넌트 안의 변수 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-
 import UserList from "./component/UserList";
+import {useRef} from "react";
 
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  // useRef를 사용하여 변수를 관리.
+  // useRef로 관리하는 변수는, 값이 바뀌어도 컴포넌트가 리렌더링 되지 않는다.
+  // useRef로 관리하는 변수는, 설정 후 바로 조회가 가능하지만,
+  // 일반 컴포넌트는 렌더링이 된 이후에 상태를 조회할 수 있다.
+  const nextId = useRef(4);
+  const onCreate = () => {
+    nextId.current += 1;
+  }
+
   return (
     <div className="App">
-      <UserList></UserList>
+      <UserList users={users}></UserList>
     </div>
   );
 }
--- src/component/UserList.jsx
+++ src/component/UserList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+
 function User({ user }) {
 	return (
 		<div>
@@ -8,25 +10,8 @@ function User({ user }) {
 	);
 }
 
-function UserList() {
-	const users = [
-		{
-			id: 1,
-			username: 'velopert',
-			email: 'public.velopert@gmail.com'
-		},
-		{
-			id: 2,
-			username: 'tester',
-			email: 'tester@example.com'
-		},
-		{
-			id: 3,
-			username: 'liz',
-			email: 'liz@example.com'
-		}
-	];
 
+function UserList({users}) {
 	return (
 		<div>
 			{users.map(user => (
